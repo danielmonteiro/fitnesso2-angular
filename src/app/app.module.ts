fff9618358aa915571b4339d3875dfbfc1b5a8b0
danielmonteiro <danielpinhomonteiro@gmail.com>
2017-08-30 16:06:48 -0300

    Novo layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MaterializeModule } from 'angular2-materialize';
+import { routing } from './app.routing';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LocalizacaoComponent } from './localizacao/localizacao.component';
+import { ContatoComponent } from './contato/contato.component';
+import { SobreNosComponent } from './sobre-nos/sobre-nos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LocalizacaoComponent,
+    ContatoComponent,
+    SobreNosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterializeModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
